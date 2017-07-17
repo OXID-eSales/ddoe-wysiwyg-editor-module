d4745b98a44f9d1aa7cb61b94c33cb05a737f218
Robert Blank <robert.blank@oxid-esales.com>
2017-07-17 14:38:07 +0200

    Update file headers

--- build/js/base/helper.js
+++ build/js/base/helper.js
@@ -1,13 +1,22 @@
 /**
- * This Software is the property of OXID eSales and is protected
- * by copyright law.
+ * This file is part of OXID eSales WYSIWYG module.
  *
- * Any unauthorized use of this software will be prosecuted by
- * civil and criminal law.
+ * OXID eSales WYSIWYG module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales WYSIWYG module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales WYSIWYG module.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @link      http://www.oxid-esales.com
  * @copyright (C) OXID eSales AG 2003-2017
- * @version   OXID eSales Summernote
+ * @version   OXID eSales WYSIWYG
  */
 
 ;(function ( $, global )
