33e139f6398bad4c4b0c10536e8f11b98e17c80b
Steffen Winde <inserv@winde-ganzig.de>
2021-02-27 12:31:19 +0100

    pfad to summernode-grid.js add

--- build/grunt/uglify.js
+++ build/grunt/uglify.js
@@ -33,6 +33,7 @@ module.exports = {
                 "build/vendor/summernote/js/summernote.js",
                 "build/vendor/summernote/js/plugins/ddmedia.summernote.js",
                 "build/vendor/summernote/js/plugins/smarty.summernote.js",
+                "build/vendor/summernote/js/plugins/summernote-grid.js",
                 "build/js/backend.js"
             ],
 
