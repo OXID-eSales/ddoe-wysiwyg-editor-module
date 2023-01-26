01dfd8a83276f2ed1edb97855bb91411fc56e8ad
Anton Fedurtsya <anton@fedurtsya.com>
2023-01-26 12:48:42 +0200

    OXDEV-5537 Add codeception tests skeleton
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

---
+++ tests/Codeception/_support/AcceptanceTester.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\WysiwygModule\Tests\Codeception;
+
+use OxidEsales\Codeception\Page\Home;
+
+final class AcceptanceTester extends \Codeception\Actor
+{
+    use _generated\AcceptanceTesterActions;
+
+    /**
+     * Open shop first page.
+     */
+    public function openShop(): Home
+    {
+        $I        = $this;
+        $homePage = new Home($I);
+        $I->amOnPage($homePage->URL);
+
+        return $homePage;
+    }
+}
