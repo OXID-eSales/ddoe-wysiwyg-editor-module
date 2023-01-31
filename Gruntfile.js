ae1c348bba1f223e02f688f82990113ecc0b4391
Anton Fedurtsya <anton@fedurtsya.com>
2023-01-31 15:27:57 +0200

    OXDEV-5537 Update license pointers in files
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,6 @@
 /**
- * This file is part of OXID eSales WYSIWYG module.
- *
- * OXID eSales WYSIWYG module is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * OXID eSales WYSIWYG module is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with OXID eSales WYSIWYG module.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @link      http://www.oxid-esales.com
- * @copyright (C) OXID eSales AG 2003-2017
- * @version   OXID eSales WYSIWYG
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
  */
 
 module.exports = function( grunt ) {
