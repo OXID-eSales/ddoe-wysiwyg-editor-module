a8d84d53f9ccfcce8a92f0d14f74101643f4492a
srikaran <balach.srikaran@digidesk.de>
2023-08-23 13:00:47 +0200

    OEVE-215 - make media library available in summernote editor

--- build/vendor/summernote/plugin/ddmedia.summernote.js
+++ build/vendor/summernote/plugin/ddmedia.summernote.js
@@ -21,21 +21,24 @@
                         {
                             className: 'btn-info',
                             contents: '<i class="fa fa-file-image-o"></i>',
-                            click: function ( e )
-                            {
-                                MediaLibrary.open( /image\/.*/i, function( id, file, fullpath )
-                                    {
-                                        context.invoke('editor.insertImage', fullpath, function( $image )
-                                            {
-                                                $image.css( 'max-width', '100%' );
-                                                $image.attr( 'data-filename', file );
-                                                $image.attr( 'data-filepath', fullpath );
-                                                $image.attr( 'data-source', 'media' );
-                                                $image.addClass( 'dd-wysiwyg-media-image' );
-                                            }
-                                        );
+                            click: function (e) {
+                                if (typeof MediaLibrary === 'undefined') {
+                                    if (top.basefrm && top.basefrm.OverlayInstance) {
+                                        top.basefrm.OverlayInstance.showOverlay(context);
                                     }
-                                );
+                                } else {
+                                    MediaLibrary.open(/image\/.*/i, function (id, file, fullpath) {
+                                            context.invoke('editor.insertImage', fullpath, function ($image) {
+                                                    $image.css('max-width', '100%');
+                                                    $image.attr('data-filename', file);
+                                                    $image.attr('data-filepath', fullpath);
+                                                    $image.attr('data-source', 'media');
+                                                    $image.addClass('dd-wysiwyg-media-image');
+                                                }
+                                            );
+                                        }
+                                    );
+                                }
                             }
                         }
                     );
