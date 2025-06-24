46ee6eb29aeec217eb3c8bb6ff1b0cc5a595fa9d
Anton Fedurtsya <anton@fedurtsya.com>
2025-06-24 12:43:47 +0300

    OEVE-240 Cleanup not used functionality
    Currently, MediaLibrary is not accessable here as its described, and this part is never triggered. This will be improved and reworked in the future.

--- node_build/js/summernote/plugins/media-library.js
+++ node_build/js/summernote/plugins/media-library.js
@@ -16,20 +16,8 @@ function ddmedia(context) {
             contents: '<i class="fa fa-file-image-o fa-file-image"></i>',
             tooltip: lang.image.image,
             click: function () {
-                if (typeof MediaLibrary === 'undefined') {
-                    if (top.basefrm && top.basefrm.OverlayInstance) {
-                        top.basefrm.OverlayInstance.showOverlay(context);
-                    }
-                } else {
-                    MediaLibrary.open(/image\/.*/i, function (id, file, fullpath) {
-                        context.invoke('editor.insertImage', fullpath, function ($image) {
-                            $image.css('max-width', '100%');
-                            $image.attr('data-filename', file);
-                            $image.attr('data-filepath', fullpath);
-                            $image.attr('data-source', 'media');
-                            $image.addClass('dd-wysiwyg-media-image');
-                        });
-                    });
+                if (top.basefrm && top.basefrm.OverlayInstance) {
+                    top.basefrm.OverlayInstance.showOverlay(context);
                 }
             }
         });
