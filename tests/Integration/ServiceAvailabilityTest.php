3e55b231acf5efd385f200af4ebd4c1e03627c17
Anton Fedurtsya <anton@fedurtsya.com>
2025-06-26 10:08:46 +0300

    OEVE-240 Add service availability intergration test
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

---
+++ tests/Integration/ServiceAvailabilityTest.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\WysiwygModule\Tests\Integration;
+
+use OxidEsales\EshopCommunity\Internal\Container\ContainerBuilderFactory;
+use OxidEsales\EshopCommunity\Tests\Integration\IntegrationTestCase;
+use OxidEsales\WysiwygModule\HtmlFilter\HtmlFilterInterface;
+use OxidEsales\WysiwygModule\HtmlFilter\HtmlRemoverInterface;
+use OxidEsales\WysiwygModule\Service\EditorRendererInterface;
+use OxidEsales\WysiwygModule\Service\SettingsInterface;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+
+class ServiceAvailabilityTest extends IntegrationTestCase
+{
+    private static $cachedContainer;
+    private static $decorations;
+
+    public static function setUpBeforeClass(): void
+    {
+        $containerBuilder = (new ContainerBuilderFactory())->create();
+        $container = $containerBuilder->getContainer();
+        foreach ($container->getDefinitions() as $id => $definition) {
+            $definition->setPublic(true);
+            if ($decorated = $definition->getDecoratedService()) {
+                self::$decorations[reset($decorated)][] = $id;
+            }
+        }
+        $container->compile(true);
+
+        self::$cachedContainer = $container;
+    }
+
+    #[DataProvider('serviceAvailabilityDataProvider')]
+    #[Test]
+    public function servicesAvailable(string $serviceName): void
+    {
+        $service = self::$cachedContainer->get($serviceName);
+        $this->assertInstanceOf($serviceName, $service);
+    }
+
+    #[DataProvider('serviceDecorationProvider')]
+//    #[Test]
+    public function servicesDecorated(string $serviceName, array $expectedDecorations): void
+    {
+        $decorations = self::$decorations[$serviceName];
+        foreach ($expectedDecorations as $oneExpectedDecoration) {
+            $this->assertContains($oneExpectedDecoration, $decorations);
+        }
+    }
+
+    public static function serviceDecorationProvider(): \Generator
+    {
+    }
+
+    public static function serviceAvailabilityDataProvider(): array
+    {
+        return [
+            // HtmlFilter
+            [HtmlFilterInterface::class],
+            [HtmlRemoverInterface::class],
+
+            // Service
+            [EditorRendererInterface::class],
+            [SettingsInterface::class],
+        ];
+    }
+}
