f3770512f1de6b0c27d301dc278a41e1e85b4755
Anton Fedurtsya <anton@fedurtsya.com>
2026-09-04 15:13:13 +0300

    OXDEV-9850 Extract integration part of the factory test to Integration directory
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

---
+++ tests/Integration/Migration/Factory/MigrationReporterFactoryTest.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+namespace OxidEsales\WysiwygModule\Tests\Integration\Migration\Factory;
+
+use OxidEsales\EshopCommunity\Internal\Transition\Utility\ContextInterface;
+use OxidEsales\EshopCommunity\Tests\Integration\IntegrationTestCase;
+use OxidEsales\WysiwygModule\Migration\DTO\MigrationReportInterface;
+use OxidEsales\WysiwygModule\Migration\Factory\MigrationReporterFactory;
+use OxidEsales\WysiwygModule\Migration\Factory\MigrationReporterFactoryInterface;
+use OxidEsales\WysiwygModule\Migration\Service\MediaMigrationResultFilterInterface;
+use PHPUnit\Framework\Attributes\Test;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MigrationReporterFactoryTest extends IntegrationTestCase
+{
+    private const REPORT_FILE_NAME = 'media-migration.csv';
+
+    private string $logDirectory;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->logDirectory = sys_get_temp_dir() . '/wysiwyg-log-' . uniqid();
+        mkdir($this->logDirectory);
+    }
+
+    public function tearDown(): void
+    {
+        array_map('unlink', glob($this->logDirectory . '/*') ?: []);
+        rmdir($this->logDirectory);
+
+        parent::tearDown();
+    }
+
+    #[Test]
+    public function relativeReportFileGoesToTheShopLogDirectory(): void
+    {
+        $sut = $this->getSut(
+            context: $this->givenShopLogFileIn($this->logDirectory)
+        );
+
+        $reporter = $sut->create(self::REPORT_FILE_NAME);
+        $reporter->report(
+            $this->createStub(MigrationReportInterface::class),
+            $this->createStub(OutputInterface::class)
+        );
+
+        $this->assertFileExists($this->logDirectory . '/' . self::REPORT_FILE_NAME);
+    }
+
+    #[Test]
+    public function absoluteReportFileIsWrittenWhereItWasAskedFor(): void
+    {
+        $absoluteReportPath = sys_get_temp_dir() . '/wysiwyg-migration-' . uniqid() . '.csv';
+
+        $sut = $this->getSut(
+            context: $this->givenShopLogFileIn($this->logDirectory)
+        );
+
+        try {
+            $reporter = $sut->create($absoluteReportPath);
+            $reporter->report(
+                $this->createStub(MigrationReportInterface::class),
+                $this->createStub(OutputInterface::class)
+            );
+
+            $this->assertFileExists($absoluteReportPath);
+            $this->assertFileDoesNotExist($this->logDirectory . '/' . basename($absoluteReportPath));
+        } finally {
+            @unlink($absoluteReportPath);
+        }
+    }
+
+    private function givenShopLogFileIn(string $directory): ContextInterface
+    {
+        return $this->createConfiguredStub(
+            ContextInterface::class,
+            ['getLogFilePath' => $directory . '/oxideshop.log']
+        );
+    }
+
+    private function getSut(
+        ?ContextInterface $context = null,
+        ?MediaMigrationResultFilterInterface $resultFilter = null,
+    ): MigrationReporterFactoryInterface {
+        $context ??= $this->get(ContextInterface::class);
+        $resultFilter ??= $this->get(MediaMigrationResultFilterInterface::class);
+
+        return new MigrationReporterFactory(
+            context: $context,
+            resultFilter: $resultFilter,
+        );
+    }
+}
--- tests/Unit/Migration/Factory/MigrationReporterFactoryTest.php
+++ tests/Unit/Migration/Factory/MigrationReporterFactoryTest.php
@@ -9,9 +9,7 @@ declare(strict_types=1);
 
 namespace OxidEsales\WysiwygModule\Tests\Unit\Migration\Factory;
 
-use org\bovigo\vfs\vfsStream;
 use OxidEsales\EshopCommunity\Internal\Transition\Utility\ContextInterface;
-use OxidEsales\WysiwygModule\Migration\DTO\MigrationReportInterface;
 use OxidEsales\WysiwygModule\Migration\Factory\MigrationReporterFactory;
 use OxidEsales\WysiwygModule\Migration\Factory\MigrationReporterFactoryInterface;
 use OxidEsales\WysiwygModule\Migration\Reporter\CsvFileMigrationReporter;
@@ -20,22 +18,9 @@ use OxidEsales\WysiwygModule\Migration\Service\MediaMigrationResultFilterInterfa
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Console\Output\OutputInterface;
 
 class MigrationReporterFactoryTest extends TestCase
 {
-    private const REPORT_FILE_NAME = 'media-migration.csv';
-    private const LOG_DIRECTORY = 'log';
-    private const LOG_FILE_PATH = 'vfs://root/' . self::LOG_DIRECTORY . '/oxideshop.log';
-    private const REPORT_IN_LOG_DIRECTORY = 'vfs://root/' . self::LOG_DIRECTORY . '/' . self::REPORT_FILE_NAME;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        vfsStream::setup('root', null, [self::LOG_DIRECTORY => []]);
-    }
-
     public static function noReportFileDataProvider(): \Generator
     {
         yield 'no option given' => ['reportFilePath' => null];
@@ -58,50 +43,17 @@ class MigrationReporterFactoryTest extends TestCase
     {
         $sut = $this->getSut();
 
-        $reporter = $sut->create(self::REPORT_FILE_NAME);
+        $reporter = $sut->create(uniqid('exampleFilePath'));
 
         $this->assertInstanceOf(CsvFileMigrationReporter::class, $reporter);
     }
 
-    #[Test]
-    public function relativeReportFileIsWrittenToTheShopLogDirectory(): void
-    {
-        $sut = $this->getSut();
-
-        $reporter = $sut->create(self::REPORT_FILE_NAME);
-        $reporter->report(
-            $this->createStub(MigrationReportInterface::class),
-            $this->createStub(OutputInterface::class)
-        );
-
-        $this->assertFileExists(self::REPORT_IN_LOG_DIRECTORY);
-    }
-
-    #[Test]
-    public function absoluteReportFileIsWrittenWhereItWasAskedFor(): void
-    {
-        // an absolute path cannot be expressed as a vfsstream url, so this one branch needs a real file
-        $path = sys_get_temp_dir() . '/wysiwyg-migration-' . uniqid() . '.csv';
-        $sut = $this->getSut();
-
-        try {
-            $reporter = $sut->create($path);
-            $reporter->report(
-                $this->createStub(MigrationReportInterface::class),
-                $this->createStub(OutputInterface::class)
-            );
-
-            $this->assertFileExists($path);
-            $this->assertFileDoesNotExist('vfs://root/' . self::LOG_DIRECTORY . '/' . basename($path));
-        } finally {
-            @unlink($path);
-        }
-    }
-
-    private function getSut(): MigrationReporterFactoryInterface
-    {
-        $context = $this->createConfiguredStub(ContextInterface::class, ['getLogFilePath' => self::LOG_FILE_PATH]);
-        $resultFilter = $this->createStub(MediaMigrationResultFilterInterface::class);
+    private function getSut(
+        ?ContextInterface $context = null,
+        ?MediaMigrationResultFilterInterface $resultFilter = null,
+    ): MigrationReporterFactoryInterface {
+        $context ??= $this->createStub(ContextInterface::class);
+        $resultFilter ??= $this->createStub(MediaMigrationResultFilterInterface::class);
 
         return new MigrationReporterFactory(
             context: $context,
