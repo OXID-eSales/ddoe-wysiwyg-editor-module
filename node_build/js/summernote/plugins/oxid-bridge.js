70d35f60dee8f4c5c9e1c4ed94d315084a3add17
Anton Fedurtsya <anton@fedurtsya.com>
2026-09-17 15:05:33 +0300

    OXDEV-10694 Fix incorrect handling of media image urls
    The regex was too strict on expecting exact order of attributes in media library imported image. This caused some urls to stay as urls instead of being converted to oeMediaUrl anchors.
    
    Signed-off-by: Anton Fedurtsya <anton@fedurtsya.com>

--- node_build/js/summernote/plugins/oxid-bridge.js
+++ node_build/js/summernote/plugins/oxid-bridge.js
@@ -3,14 +3,44 @@
  * See LICENSE file for license details.
  */
 
+var MEDIA_IMAGE_CLASS = 'dd-wysiwyg-media-image';
+
+function isTextarea(node) {
+    return node && node.nodeName.toUpperCase() === 'TEXTAREA';
+}
+
+function getTagAttributeValue(tag, attributeName) {
+    var match = tag.match(new RegExp('\\s' + attributeName + '\\s*=\\s*"([^"]*)"', 'i'));
+
+    return match ? match[1] : null;
+}
+
+function checkTagHasMediaImageClass(tag) {
+    var classAttribute = getTagAttributeValue(tag, 'class') || '';
+
+    return classAttribute.split(/\s+/).indexOf(MEDIA_IMAGE_CLASS) !== -1;
+}
+
+function rewriteMediaImageSourceInMarkup(markup, buildSourceCallback) {
+    return markup.replace(/<img\s[^>]*>/gi, function (tag) {
+        var id = getTagAttributeValue(tag, 'data-id');
+
+        if (!id || !checkTagHasMediaImageClass(tag)) {
+            return tag;
+        }
+
+        var newSourceValue = buildSourceCallback(id);
+
+        // replaces to new source value in the src attribute of the specific tag string
+        return tag.replace(/(\ssrc\s*=\s*")[^"]*(")/i, function (match, start, end) {
+            return start + newSourceValue + end;
+        });
+    });
+}
+
 export function injectOxidBridge(mediaModule) {
-
-    var isTextarea = function (node) {
-        return node && node.nodeName.toUpperCase() === 'TEXTAREA';
-    };
-
     $.extend($.summernote.dom, {
-        value: function($node, stripLinebreaks) {
+        value: function ($node, stripLinebreaks) {
             var val;
 
             if (isTextarea($node[0])) {
@@ -18,16 +48,14 @@ export function injectOxidBridge(mediaModule) {
 
                 // fix tags double quotes within attributes
                 var regex = new RegExp(/(=\s*")([^">]*)(\{\{([^\}\}]|\}[^\}]|[^\}]\})*\}\})([^">]*)(")/gi);
-                val = val.replace(regex, function(text, start, attr_before, smarty, smarty_inner, attr_after, end) {
+                val = val.replace(regex, function (text, start, attr_before, smarty, smarty_inner, attr_after, end) {
                     smarty = smarty.replace(/\\"/g, '\'').replace(/"/g, '\'');
                     return (start + attr_before + smarty + attr_after + end);
                 });
 
                 // switch twig function call with media url
-                var regexMediaUrl = new RegExp(/<img[^>]*src=\s*"([^"]+)"[^>]*data-id=\s*"([^"]+)"[^>]*class=\s*"[^">]*dd-wysiwyg-media-image[^">]*"[^>]*>/gi);
-                val = val.replace(regexMediaUrl, function(text, src, id) {
-                    text = text.replace(src, mediaModule.getMediaUrl(id));
-                    return text;
+                val = rewriteMediaImageSourceInMarkup(val, function (id) {
+                    return mediaModule.getMediaUrl(id);
                 });
             } else {
                 val = $node.html();
@@ -41,7 +69,7 @@ export function injectOxidBridge(mediaModule) {
         },
 
         html: function ($node, isNewlineOnBlock) {
-            var markup = this.value( $node );
+            var markup = this.value($node);
 
             if (isNewlineOnBlock) {
                 var regexTag = /<(\/?)(\b(?!!)[^>\s]*)(.*?)(\s*\/?>)/g;
@@ -49,22 +77,19 @@ export function injectOxidBridge(mediaModule) {
                 markup = markup.replace(regexTag, function (match, endSlash, name) {
                     name = name.toUpperCase();
 
-                    var isEndOfInlineContainer = /^DIV|^TD|^TH|^P|^LI|^H[1-7]/.test( name ) && !!endSlash;
-                    var isBlockNode            = /^BLOCKQUOTE|^TABLE|^TBODY|^TR|^HR|^UL|^OL/.test( name );
+                    var isEndOfInlineContainer = /^DIV|^TD|^TH|^P|^LI|^H[1-7]/.test(name) && !!endSlash;
+                    var isBlockNode = /^BLOCKQUOTE|^TABLE|^TBODY|^TR|^HR|^UL|^OL/.test(name);
 
                     return match + ((isEndOfInlineContainer || isBlockNode) ? '\n' : '');
                 });
 
-                markup = $.trim( markup );
+                markup = $.trim(markup);
             }
 
             // set media smarty or twig tags
-            markup = markup.replace(
-                /<img[^>]*src=\s*"([^"]+)"[^>]*data-id=\s*"([^">]+)"[^>]*class=\s*"[^">]*dd-wysiwyg-media-image[^">]*"[^>]*>/gi,
-                function(tag, src, id) {
-                    return tag.replace( src, "{{oeMediaUrl('" + id + "')}}" );
-                }
-            );
+            markup = rewriteMediaImageSourceInMarkup(markup, function (id) {
+                return "{{oeMediaUrl('" + id + "')}}";
+            });
 
             return markup;
         }
